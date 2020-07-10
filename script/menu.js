7447062a9a2fbc823b7c01c6951deac3202e038c
Alexandru Bejenaru <54982557+AlexBeje@users.noreply.github.com>
2020-07-10 12:55:22 +0200

    feat: creation of the skills page (mobile)

--- script/menu.js
+++ script/menu.js
@@ -2,19 +2,23 @@ const body = document.querySelector("body");
 
 const burgerIcon = document.querySelector(".nav__icon--burger");
 const closeIcon = document.querySelector(".nav__icon--close");
-const navItems = document.querySelector(".nav__items")
+const navItems = document.querySelector(".nav__items");
+
+const projectsPage = document.querySelector("#projects-page");
+const skillsPage = document.querySelector("#skills-page");
 
 function toggleMenu() {
   if (burgerIcon.classList.contains("d-block")) {
     closeIcon.className = "nav__icon--close d-block";
     burgerIcon.className = "nav__icon--burger d-none";
     navItems.className = "nav__items d-flex";
-    body.style = "overflow: hidden";
-
+    projectsPage.style = "display: none;";
+    skillsPage.style = "display: none;";
   } else {
     closeIcon.className = "nav__icon--close d-none";
     burgerIcon.className = "nav__icon--burger d-block";
     navItems.className = "nav__items d-none";
-    body.style = "overflow: auto";
+    projectsPage.style = "display: block;";
+    skillsPage.style = "display: block;";
   }
 }
